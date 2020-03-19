c392c39497e1bc1a18d2dd32cc08c33741b55d3c
Greg Esmond <gernby2002@yahoo.com>
2020-03-19 12:49:45 -0500

    enhanced for RAV4

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -43,7 +43,6 @@ DESIRES = {
 def calc_states_after_delay(states, v_ego, steer_angle, curvature_factor, steer_ratio, delay):
   states[0].x = v_ego * delay
   states[0].psi = v_ego * curvature_factor * math.radians(steer_angle) / steer_ratio * delay
-  if abs(steer_angle) > 4: states[0].delta = math.radians(steer_angle) / steer_ratio
   return states
 
 
@@ -163,6 +162,9 @@ class PathPlanner():
 
     # account for actuation delay
     self.cur_state = calc_states_after_delay(self.cur_state, v_ego, angle_steers - angle_offset, curvature_factor, VM.sR, CP.steerActuatorDelay)
+
+    if abs(angle_steers - angle_offset) > 4 and CP.lateralTuning.which() == 'pid': 
+      self.cur_state[0].delta = math.radians(angle_steers - angle_offset) / VM.sR
 
     v_ego_mpc = max(v_ego, 5.0)  # avoid mpc roughness due to low speed
     self.libmpc.run_mpc(self.cur_state, self.mpc_solution,
